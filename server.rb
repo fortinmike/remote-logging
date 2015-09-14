7ea77fcecbe7f8b11683629fffc3b3b9b5d36554
Michaël Fortin <fortinmike@irradiated.net>
2015-09-14 08:42:51 -0400

    Added GET / route to indicate that the server is working.

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  "<h2>remote-logging server ready to receive messages!</h2><br/>Perform a POST on / with something to log as the request body."
+end
+
 post '/' do
   puts request.body.read.to_s
 end
